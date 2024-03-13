b19b268882a994c5c5a0c84d60ed9d1e18ffc3be
Adrita Bhattacharjee <98798926+Adrita12@users.noreply.github.com>
2024-03-13 15:37:22 +0530

    added easter eggs

--- script.js
+++ script.js
@@ -10,5 +10,68 @@ preloading();
 
 
    
+function hint1(){
+    document.addEventListener('selectionchange', function() {
+        var selection = document.getSelection();
+        var selectedText = selection ? selection.toString() : null;
+    
+        if (!selectedText) {
+            hidePopup();
+            return;
+        }
+
+        if (selectedText.trim() === 'MYSTIC MAP') {
+            showPopup("hehe hint recieved");
+        } else {
+            hidePopup();
+        }
+    });
+    
+    function showPopup(message) {
+        alert(message);
+    }
+    
+    function hidePopup() {
+    }
+}
+hint1();
+
+
+function hint2(){
+    var typedText = '';
+
+    document.addEventListener('keydown', function(event) {
+        typedText += event.key.toUpperCase();
+        if (typedText.includes('MYSTIC')) {
+            showPopup("hehe hint recieved");
+            typedText = '';
+        }
+    });
+    
+    function showPopup(message) {
+        alert(message);
+    }
+    
+}
+hint2();
+
+
+function hint3(){
+    function hint3() {
+        var imageElement = document.querySelector('img[alt="sponsor_hint.png"]');
+        imageElement.addEventListener('click', function() {
+            showPopup("Code is 112");
+        });
+    
+        function showPopup(message) {
+            alert(message);
+        }
+    }
+    
+    hint3();
+    
+}
+hint3();
+   
 
    
