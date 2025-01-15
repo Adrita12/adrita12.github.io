ec1d0ec8966993e8501071f05de0c207e13d3c0f
Adrita Bhattacharjee <98798926+Adrita12@users.noreply.github.com>
2025-01-15 21:05:36 +0530

    hints rermoved

--- script.js
+++ script.js
@@ -8,184 +8,184 @@ preloading();
 
 
   
-function hint1(){
-    document.addEventListener('selectionchange', function() {
-        var selection = document.getSelection();
-        var selectedText = selection ? selection.toString() : null;
+// function hint1(){
+//     document.addEventListener('selectionchange', function() {
+//         var selection = document.getSelection();
+//         var selectedText = selection ? selection.toString() : null;
     
-        if (!selectedText) {
-            hidePopup();
-            return;
-        }
+//         if (!selectedText) {
+//             hidePopup();
+//             return;
+//         }
 
-        if (selectedText.trim() === 'MYSTIC MAP') {
-            showPopup("hint1.png");
-        } 
-    });
+//         if (selectedText.trim() === 'MYSTIC MAP') {
+//             showPopup("hint1.png");
+//         } 
+//     });
     
-    function showPopup() {
-        var popup = document.createElement('div');
-        popup.style.position = 'fixed';
-        popup.style.top = '50%';
-        popup.style.left = '50%';
-        popup.style.transform = 'translate(-50%, -50%)';
-        popup.style.zIndex = '1000';
-        // popup.style.backgroundColor = 'white';
-        popup.style.padding = '10px';
-        popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+//     function showPopup() {
+//         var popup = document.createElement('div');
+//         popup.style.position = 'fixed';
+//         popup.style.top = '50%';
+//         popup.style.left = '50%';
+//         popup.style.transform = 'translate(-50%, -50%)';
+//         popup.style.zIndex = '1000';
+//         // popup.style.backgroundColor = 'white';
+//         popup.style.padding = '10px';
+//         popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
 
-        var img = document.createElement('img');
-        img.src = 'hint1.webp';  // Replace with the URL of your image
-        img.alt = 'Hint Image';
-        img.style.maxWidth = '60%';
-        img.style.height = 'auto';
+//         var img = document.createElement('img');
+//         img.src = 'hint1.webp';  // Replace with the URL of your image
+//         img.alt = 'Hint Image';
+//         img.style.maxWidth = '60%';
+//         img.style.height = 'auto';
 
-        var closeBtn = document.createElement('button');
-        closeBtn.innerText = 'Close';
-        closeBtn.style.display = 'block';
-        closeBtn.style.margin = '5px auto 0';
-        closeBtn.style.padding = '5px 10px';
-        closeBtn.style.cursor = 'pointer';
-        closeBtn.onclick = function() {
-            document.body.removeChild(popup);
-        };
+//         var closeBtn = document.createElement('button');
+//         closeBtn.innerText = 'Close';
+//         closeBtn.style.display = 'block';
+//         closeBtn.style.margin = '5px auto 0';
+//         closeBtn.style.padding = '5px 10px';
+//         closeBtn.style.cursor = 'pointer';
+//         closeBtn.onclick = function() {
+//             document.body.removeChild(popup);
+//         };
 
-        popup.appendChild(img);
-        popup.appendChild(closeBtn);
-        document.body.appendChild(popup);
-    }
-}
-hint1();
+//         popup.appendChild(img);
+//         popup.appendChild(closeBtn);
+//         document.body.appendChild(popup);
+//     }
+// }
+// hint1();
 
 
-function hint2(){
-    var typedText = '';
+// function hint2(){
+//     var typedText = '';
 
-    document.addEventListener('keydown', function(event) {
-        typedText += event.key.toUpperCase();
-        if (typedText.includes('MYSTIC')) {
-            showPopup();
-            typedText = '';
-        }
-    });
+//     document.addEventListener('keydown', function(event) {
+//         typedText += event.key.toUpperCase();
+//         if (typedText.includes('MYSTIC')) {
+//             showPopup();
+//             typedText = '';
+//         }
+//     });
     
-    function showPopup() {
-        var popup = document.createElement('div');
-        popup.style.position = 'fixed';
-        popup.style.top = '50%';
-        popup.style.left = '50%';
-        popup.style.transform = 'translate(-50%, -50%)';
-        popup.style.zIndex = '1000';
-        // popup.style.backgroundColor = 'white';
-        popup.style.padding = '10px';
-        popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+//     function showPopup() {
+//         var popup = document.createElement('div');
+//         popup.style.position = 'fixed';
+//         popup.style.top = '50%';
+//         popup.style.left = '50%';
+//         popup.style.transform = 'translate(-50%, -50%)';
+//         popup.style.zIndex = '1000';
+//         // popup.style.backgroundColor = 'white';
+//         popup.style.padding = '10px';
+//         popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
 
-        var img = document.createElement('img');
-        img.src = 'hint2.webp';  // Replace with the URL of your image
-        img.alt = 'Hint Image';
-        img.style.maxWidth = '60%';
-        img.style.height = 'auto';
+//         var img = document.createElement('img');
+//         img.src = 'hint2.webp';  // Replace with the URL of your image
+//         img.alt = 'Hint Image';
+//         img.style.maxWidth = '60%';
+//         img.style.height = 'auto';
 
-        var closeBtn = document.createElement('button');
-        closeBtn.innerText = 'Close';
-        closeBtn.style.display = 'block';
-        closeBtn.style.margin = '5px auto 0';
-        closeBtn.style.padding = '5px 10px';
-        closeBtn.style.cursor = 'pointer';
-        closeBtn.onclick = function() {
-            document.body.removeChild(popup);
-        };
+//         var closeBtn = document.createElement('button');
+//         closeBtn.innerText = 'Close';
+//         closeBtn.style.display = 'block';
+//         closeBtn.style.margin = '5px auto 0';
+//         closeBtn.style.padding = '5px 10px';
+//         closeBtn.style.cursor = 'pointer';
+//         closeBtn.onclick = function() {
+//             document.body.removeChild(popup);
+//         };
 
-        popup.appendChild(img);
-        popup.appendChild(closeBtn);
-        document.body.appendChild(popup);
-    }
-}
+//         popup.appendChild(img);
+//         popup.appendChild(closeBtn);
+//         document.body.appendChild(popup);
+//     }
+// }
 
-hint2();
+// hint2();
 
 
-function hint3() {
-    var sponsorHintImages = document.querySelectorAll('img[alt="sponsor_hint.png"]');
-    sponsorHintImages.forEach(function(imageElement) {
-        imageElement.addEventListener('click', function() {
-            showPopup("hehe hint received");
-        });
-    });
-    function showPopup() {
-        var popup = document.createElement('div');
-        popup.style.position = 'fixed';
-        popup.style.top = '50%';
-        popup.style.left = '50%';
-        popup.style.transform = 'translate(-50%, -50%)';
-        popup.style.zIndex = '1000';
-        // popup.style.backgroundColor = 'white';
-        popup.style.padding = '10px';
-        popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+// function hint3() {
+//     var sponsorHintImages = document.querySelectorAll('img[alt="sponsor_hint.png"]');
+//     sponsorHintImages.forEach(function(imageElement) {
+//         imageElement.addEventListener('click', function() {
+//             showPopup("hehe hint received");
+//         });
+//     });
+//     function showPopup() {
+//         var popup = document.createElement('div');
+//         popup.style.position = 'fixed';
+//         popup.style.top = '50%';
+//         popup.style.left = '50%';
+//         popup.style.transform = 'translate(-50%, -50%)';
+//         popup.style.zIndex = '1000';
+//         // popup.style.backgroundColor = 'white';
+//         popup.style.padding = '10px';
+//         popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
 
-        var img = document.createElement('img');
-        img.src = 'hint3.webp';  // Replace with the URL of your image
-        img.alt = 'Hint Image';
-        img.style.maxWidth = '60%';
-        img.style.height = 'auto';
+//         var img = document.createElement('img');
+//         img.src = 'hint3.webp';  // Replace with the URL of your image
+//         img.alt = 'Hint Image';
+//         img.style.maxWidth = '60%';
+//         img.style.height = 'auto';
 
-        var closeBtn = document.createElement('button');
-        closeBtn.innerText = 'Close';
-        closeBtn.style.display = 'block';
-        closeBtn.style.margin = '5px auto 0';
-        closeBtn.style.padding = '5px 10px';
-        closeBtn.style.cursor = 'pointer';
-        closeBtn.onclick = function() {
-            document.body.removeChild(popup);
-        };
+//         var closeBtn = document.createElement('button');
+//         closeBtn.innerText = 'Close';
+//         closeBtn.style.display = 'block';
+//         closeBtn.style.margin = '5px auto 0';
+//         closeBtn.style.padding = '5px 10px';
+//         closeBtn.style.cursor = 'pointer';
+//         closeBtn.onclick = function() {
+//             document.body.removeChild(popup);
+//         };
 
-        popup.appendChild(img);
-        popup.appendChild(closeBtn);
-        document.body.appendChild(popup);
-    }
-}
-hint3();
+//         popup.appendChild(img);
+//         popup.appendChild(closeBtn);
+//         document.body.appendChild(popup);
+//     }
+// }
+// hint3();
 
     
 
    
-function hint4() {
+// function hint4() {
 
-    var imageElement = document.querySelector('label[for="c3"] .icon');
-    imageElement.addEventListener('click', function() {
-        showPopup("hehe hint received");
-    });
-    function showPopup() {
-        var popup = document.createElement('div');
-        popup.style.position = 'fixed';
-        popup.style.top = '50%';
-        popup.style.left = '50%';
-        popup.style.transform = 'translate(-50%, -50%)';
-        popup.style.zIndex = '1000';
-        // popup.style.backgroundColor = 'white';
-        popup.style.padding = '10px';
-        popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+//     var imageElement = document.querySelector('label[for="c3"] .icon');
+//     imageElement.addEventListener('click', function() {
+//         showPopup("hehe hint received");
+//     });
+//     function showPopup() {
+//         var popup = document.createElement('div');
+//         popup.style.position = 'fixed';
+//         popup.style.top = '50%';
+//         popup.style.left = '50%';
+//         popup.style.transform = 'translate(-50%, -50%)';
+//         popup.style.zIndex = '1000';
+//         // popup.style.backgroundColor = 'white';
+//         popup.style.padding = '10px';
+//         popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
 
-        var img = document.createElement('img');
-        img.src = 'hint4.webp';  // Replace with the URL of your image
-        img.alt = 'Hint Image';
-        img.style.maxWidth = '60%';
-        img.style.height = 'auto';
+//         var img = document.createElement('img');
+//         img.src = 'hint4.webp';  // Replace with the URL of your image
+//         img.alt = 'Hint Image';
+//         img.style.maxWidth = '60%';
+//         img.style.height = 'auto';
 
-        var closeBtn = document.createElement('button');
-        closeBtn.innerText = 'Close';
-        closeBtn.style.display = 'block';
-        closeBtn.style.margin = '5px auto 0';
-        closeBtn.style.padding = '5px 10px';
-        closeBtn.style.cursor = 'pointer';
-        closeBtn.onclick = function() {
-            document.body.removeChild(popup);
-        };
+//         var closeBtn = document.createElement('button');
+//         closeBtn.innerText = 'Close';
+//         closeBtn.style.display = 'block';
+//         closeBtn.style.margin = '5px auto 0';
+//         closeBtn.style.padding = '5px 10px';
+//         closeBtn.style.cursor = 'pointer';
+//         closeBtn.onclick = function() {
+//             document.body.removeChild(popup);
+//         };
 
-        popup.appendChild(img);
-        popup.appendChild(closeBtn);
-        document.body.appendChild(popup);
-    }
-}
+//         popup.appendChild(img);
+//         popup.appendChild(closeBtn);
+//         document.body.appendChild(popup);
+//     }
+// }
 
-hint4();
+// hint4();
